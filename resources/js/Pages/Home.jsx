d8975dafc3c07820b25d589d3bd4640fc56d4ed2
Amrit-raj-sharma <amrit@fortaxe.com>
2025-01-21 18:09:12 +0530

    Dev - created Home Page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,5 +1,15 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import OurTutor from "../Components/OurTutor";
+import SuccessComponent from "../Components/SuccessComponent";
+import TutorCards from "../Components/TutorCards";
+
 export default function Home({name}){
     return <>
-    <h1 className="font-bold">{name}</h1>
+    <Header />
+    <SuccessComponent />
+    <TutorCards />
+    <OurTutor />
+    <Footer />
     </>
 }
