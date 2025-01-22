e3167428034fbba907af86da50c08e9c0d8b061b
Amrit-raj-sharma <amrit@fortaxe.com>
2025-01-22 18:05:20 +0530

    Dev - added rest of components on Home Page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,15 +1,23 @@
+import Blogs from "../Components/Blogs";
+import ConnectUs from "../Components/ConnectUs";
+import FindTutors from "../Components/FindTutors";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import OurTutor from "../Components/OurTutor";
+import StudentThoughts from "../Components/StudentThoughts";
 import SuccessComponent from "../Components/SuccessComponent";
 import TutorCards from "../Components/TutorCards";
 
 export default function Home({name}){
     return <>
     <Header />
+    <FindTutors />
     <SuccessComponent />
     <TutorCards />
     <OurTutor />
+    <StudentThoughts />
+    <ConnectUs />
+    <Blogs />
     <Footer />
     </>
 }
